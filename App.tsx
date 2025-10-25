9b86b3864994bf86a0ccfe1bd1d4ecbeaee0390e
dankotyt <danilkotlarov1@gmail.com>
2025-10-25 12:12:20 +0300

    chore: заменен конфиг в соответствии с измененной структурой проекта

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppNavigator } from '@/navigation/AppNavigator';
 import { AuthProvider } from '@/services/AuthProvider';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { AdvertisementProvider } from './src/services/AdvertisementContext';
+import { AdvertisementProvider } from '@services/AdvertisementContext';
 
 export default function App() {
     return (
